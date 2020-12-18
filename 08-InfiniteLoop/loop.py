4328a12045e16233e650c8030cfa5e2dbe8eb4a6
wertqyuio <ultimatenarutofan@gmail.com>
2020-12-17 18:24:30 -0800

    Came up with iterative solution early, but mismanaged the underlying data structure

--- 08-InfiniteLoop/loop.py
+++ 08-InfiniteLoop/loop.py
@@ -35,19 +35,32 @@ def second():
     lines = dict()
     line_num = 0
     visited = list()
+    reset_dict = list()
 
     for line in f:
-        lines[line_num] = line[:-1]
+        lines[line_num] = line[:-1] if line[-1] == "\n" else line
         line_num += 1
 
-    current = [0, 0]
+    current = [0, 0, 0, None]
 
-    def _process(visited, current):
-        while current[0] not in visited:
+    def _remap(field):
+        action = field[2][0]
+        if action == "nop":
+            action = "jmp"
+        elif action == "jmp":
+            action = "nop"
+        field[2][0] = action
+
+    def _process(visited, current, printvar=False):
+        while current[0] not in visited and current[0] < line_num:
+            if printvar:
+                print(current[0])
             visited.append(current[0])
+            reset_dict.append(current[1])
             process_line = lines[current[0]].split(" ")
             action = process_line[0]
             num = int(process_line[1])
+            current[2] = process_line
             if action == "nop":
                 current[0] += 1
             elif action == "acc":
@@ -55,11 +68,37 @@ def second():
                 current[0] += 1
             else:
                 current[0] += num
+        if current[3] is None:
+            current[3] = len(visited)
 
-        return current
+    def _modify_process(visited, current):
+        if current[0] == line_num:
+            return
+        current[3] -= 1
+        while len(visited) and len(visited) > current[3]:
+            latest = visited.pop()
+        _remap(current)
+        visited.append(latest)
+        current[0] = latest
+        current[1] = reset_dict[len(visited)-1]
+        action = current[2][0]
+        num = int(current[2][1])
+        if action == "acc":
+            current[1] += num
+            current[0] += 1
+        elif action == "nop":
+            current[0] += 1
+        else:
+            current[0] += num
+        _process(visited, current)
 
-    current = _process(visited, current)
-    print(visited)
+    _process(visited, current)
+    counter = 3000
+    while current[0] != line_num and counter and current[0] < line_num:
+        counter -= 1
+        _modify_process(visited, current)
+
+    print('answer is ' + str(current[1]))
 
 
 if __name__ == "__main__":
