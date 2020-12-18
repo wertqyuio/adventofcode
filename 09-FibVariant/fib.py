440eeaf28b9dfff3bb9a4e1060bba5f9e43d1d43
wertqyuio <ultimatenarutofan@gmail.com>
2020-12-18 09:18:58 -0800

    Indexing is very important, esp. relative lengths

--- 09-FibVariant/fib.py
+++ 09-FibVariant/fib.py
@@ -1,11 +1,19 @@
 def main():
-    # TO-DO: Write a function that processes numbers :D
-    # Later- write everything as a Fibonacci number basically
+    # TO-DO: Write a function that takes in numbers, and returns
+    # the first number that is not a sum of exactly 2 of previous
+    # preamble numbers.
+    # Part 2: Identifying the number and then identifying a range
+    # s.t. summing over all the numbers in range eqauls the number.
     f = open("data.txt", "r")
     nums = []
+    cum_nums = []
 
     for line in f:
         nums.append(int(line.strip('\n')))
+        if not len(cum_nums):
+            cum_nums = [0]
+        else:
+            cum_nums.append(cum_nums[-1]+nums[-1])
 
     def process_nums(preamble=25):
         current = []
@@ -25,8 +33,19 @@ def main():
                 current[idx % preamble] = nums[idx]
             idx += 1
 
-    print(process_nums())
-
+    num_breaker = process_nums()
+    sum_to_num_breaker = 0
+    idx_left = 0
+    idx_right = 1
+    while sum_to_num_breaker != num_breaker:
+        sum_to_num_breaker = cum_nums[idx_right] - cum_nums[idx_left]
+        if sum_to_num_breaker < num_breaker:
+            idx_right += 1
+        elif sum_to_num_breaker > num_breaker:
+            idx_left += 1
+    narrow_range = nums[idx_left+1:idx_right+1]
+    narrow_range.sort()
+    print(narrow_range[0]+narrow_range[-1])
 
 if __name__ == "__main__":
     main()
